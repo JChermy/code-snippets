4c66185b4bb13cebed04c3909c6b41be0a588066
Jchermy <907937376@qq.com>
2018-07-04 15:42:06 +0800

    add deepcopy code

---
+++ deepCopy_oneParam.js
@@ -0,0 +1,25 @@
+function deepCopy(obj) {
+    let result = {}
+    let keys = Object.keys(obj),
+         key = null,
+         temp = null;
+    for(let i =0,len=keys.length;i<len;i++) {
+        key = keys[i];
+        temp = obj[key];
+        if(temp && typeof temp === 'object') {
+            result.key = deepCopy(temp);
+        }else {
+            result.key = temp;
+        }
+    }
+    return result;
+}
+var obj = {
+     a:1,
+     b:null,
+     c:undefined,
+     symbol: Symbol('foo'),
+     f: function(a, b) { return a+b;},
+     s: "string"
+}
+var copy = deepCopy(obj)
---
+++ deepCopy_twoParams.js
@@ -0,0 +1,27 @@
+function deepCopy(parent, copy) {
+    copy = copy || {}
+    for(var i in parent) {
+        if(parent.hasOwnProperty(i)) {
+            if(typeof parent[i]  === 'object') {
+                copy[i] = Array.isArray(parent[i]) ? [] : {}
+                deepCopy(parent[i], copy[i])
+            }else {
+                copy[i] = parent[i];
+            }
+        }
+    }
+    return copy;
+}
+var obj = {
+    a:1,
+    b:"str",
+    c:Symbol("c"),
+    d:function(x,y){
+        return x+y;
+    },
+    e:undefined,
+    f:null,
+    g: [1,2,3]
+}
+ 
+var obj2 = deepCopy(obj);
